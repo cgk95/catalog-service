0e07814608ae38a06ef2fd5a48b8ba106434e5ca
cgk95 <kjkj5868@gmail.com>
2024-04-25 17:18:01 +0900

    feat :: 스프링 시큐리티와 OAuth2를 이용한 역할에 따른 API 보호(명령형)

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,33 +1,77 @@
 package com.polarbookshop.catalogservice;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.polarbookshop.catalogservice.domain.Book;
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
         webEnvironment = WebEnvironment.RANDOM_PORT
 )
 @ActiveProfiles("integration")
+@Testcontainers
 class CatalogServiceApplicationTests {
+    // Customer
+    private static KeycloakToken bjornTokens;
+    // Customer and employee
+    private static KeycloakToken isabelleTokens;
 
     @Autowired
     private WebTestClient webTestClient;
 
+
+    @Container
+    private static final KeycloakContainer keycloakContainer = new KeycloakContainer("quay.io/keycloak/keycloak:23.0")
+            .withRealmImportFile("/test-realm-config.json");
+
+    /**
+     * @param registry 키클록 발행자 URI 가 테스트 키클록 인스턴스를 가리키도록 변경
+     */
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri",
+                () -> keycloakContainer.getAuthServerUrl() + "realms/PolarBookshop");
+    }
+
+    @BeforeAll
+    static void generateAccessTokens() {
+        WebClient webClient = WebClient.builder() // 키클록을 호출하기 위한 WebClient 통신
+                .baseUrl(keycloakContainer.getAuthServerUrl() + "/realms/PolarBookshop/protocol/openid-connect/token")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .build();
+
+        isabelleTokens = authenticateWith("isabelle", "password", webClient);
+        bjornTokens = authenticateWith("bjorn", "password", webClient);
+    }
+
     @Test
-    @DisplayName("카탈로그에 책을 추가하는데 성공하는 통합테스트")
-    void whenPostRequestThenBookCreated() {
-        var expectedBook = Book.of("1234567890", "Test Book", "author", 9.99,"제이펍");
+    @DisplayName("점원이 카탈로그에 책을 추가하는데 성공하는 통합테스트")
+    void whenPostRequestAuthorizedThenBookCreated() {
+        var expectedBook = Book.of("1234567890", "Test Book", "author", 9.99, "제이펍");
 
         webTestClient
                 .post()
                 .uri("/books")
+                .headers(headers ->
+                        headers.setBearerAuth(isabelleTokens.accessToken()))
                 .bodyValue(expectedBook)
                 .exchange()
                 .expectStatus().isCreated()
@@ -35,7 +79,54 @@ class CatalogServiceApplicationTests {
                     assertThat(actualBook).isNotNull();
                     assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
                 });
-
     }
 
+    @Test
+    @DisplayName("손님이 카탈로그에 책을 추가하는데 실패하는 통합테스트")
+    void whenPostRequestUnAuthorizedThenBook403() {
+        var expectedBook = Book.of("1234567890", "Test Book", "author", 9.99, "제이펍");
+
+        webTestClient
+                .post()
+                .uri("/books")
+                .headers(headers ->
+                        headers.setBearerAuth(bjornTokens.accessToken()))
+                .bodyValue(expectedBook)
+                .exchange()
+                .expectStatus().isForbidden();
+    }
+
+    @Test
+    void whenPostRequestUnauthenticatedThen401() {
+        var expectedBook = Book.of("1234567890", "Test Book", "author", 9.99, "제이펍");
+
+        webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(expectedBook)
+                .exchange()
+                .expectStatus().isUnauthorized();
+    }
+
+    private static KeycloakToken authenticateWith(String username, String password, WebClient webClient) {
+        return webClient
+                .post()
+                .body(BodyInserters.fromFormData("grant_type", "password")
+                        .with("client_id", "polar-test")
+                        .with("username", username)
+                        .with("password", password)
+                )
+                .retrieve()
+                .bodyToMono(KeycloakToken.class)
+                .block(); // 결과가 나올 때까지 막고 기다린다.
+    }
+
+    private record KeycloakToken(String accessToken) {
+
+        @JsonCreator // 이 생성자를 통해 역직렬화하도록 지시
+        private KeycloakToken(@JsonProperty("access_token") final String accessToken) {
+            this.accessToken = accessToken;
+        }
+
+    }
 }
--- src/test/java/com/polarbookshop/catalogservice/controller/BookControllerMvcTests.java
+++ src/test/java/com/polarbookshop/catalogservice/controller/BookControllerMvcTests.java
@@ -2,6 +2,7 @@ package com.polarbookshop.catalogservice.controller;
 
 import static org.mockito.BDDMockito.given;
 
+import com.polarbookshop.catalogservice.config.SecurityConfig;
 import com.polarbookshop.catalogservice.domain.BookNotFoundException;
 import com.polarbookshop.catalogservice.domain.BookService;
 import org.junit.jupiter.api.DisplayName;
@@ -9,17 +10,26 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+
 @WebMvcTest(BookController.class)
+@Import(SecurityConfig.class)
 public class BookControllerMvcTests {
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private BookService bookService;
+
+    @MockBean
+    JwtDecoder jwtDecoder;
 
     @Test
     @DisplayName("카탈로그에 책이 존재하지 않을 때 404 응답을 반환하는 테스트")
@@ -29,7 +39,31 @@ public class BookControllerMvcTests {
                 .willThrow(BookNotFoundException.class);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/books/{isbn}", isbn))
-               .andExpect(MockMvcResultMatchers.status().isNotFound());
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
 
     }
+
+    @Test
+    void whenDeleteBookWithEmployeeRoleThenShouldReturn204() throws Exception {
+        var isbn = "7373731394";
+        mockMvc.perform(MockMvcRequestBuilders.delete("/books/{isbn}", isbn)
+                        .with(SecurityMockMvcRequestPostProcessors.jwt().authorities(new SimpleGrantedAuthority("ROLE_employee"))))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+    }
+
+    @Test
+    void whenDeleteBookWithCustomerRoleThenShouldReturn403() throws Exception {
+        var isbn = "7373731394";
+        mockMvc.perform(MockMvcRequestBuilders.delete("/books/{isbn}", isbn)
+                        .with(SecurityMockMvcRequestPostProcessors.jwt().authorities(new SimpleGrantedAuthority("ROLE_customer"))))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    void whenDeleteBookNotAuthenticatedThenShouldReturn401() throws Exception {
+        var isbn = "7373731394";
+        mockMvc.perform(MockMvcRequestBuilders.delete("/books/{isbn}", isbn))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
 }
